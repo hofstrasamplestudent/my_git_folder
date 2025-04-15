424c148661ce232bbf56b77f89f1725fe20ecf90
hofstrasamplestudent <amarazayn@myyahoo.com>
2025-04-15 00:38:06 +0000

    test

---
+++ assignment5/script.js
@@ -0,0 +1,12 @@
+// Display a console message
+console.log("CSS + JavaScript is powerful!");
+
+// Show an alert when the page loads
+window.onload = function () {
+    alert("Welcome to Assignment 5 with JavaScript!");
+};
+
+// Function to toggle a dark/light theme
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+}
